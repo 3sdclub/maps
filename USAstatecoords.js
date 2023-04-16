ed82a50bfa70af6a8733678430a59cdee9543330
3sdclub <130933939+3sdclub@users.noreply.github.com>
2023-04-16 16:48:03 -0400

    Add files via upload

---
+++ USAstatecoords.js
@@ -0,0 +1,308 @@
+var usaStates = [
+ {
+  "State Code": "AL",
+  "State": "Alabama",
+  "Latitude": 32.3182314,
+  "Longitude": -86.902298
+ },
+ {
+  "State Code": "AK",
+  "State": "Alaska",
+  "Latitude": 64.2008413,
+  "Longitude": -149.4936733
+ },
+ {
+  "State Code": "AZ",
+  "State": "Arizona",
+  "Latitude": 34.0489281,
+  "Longitude": -111.0937311
+ },
+ {
+  "State Code": "AR",
+  "State": "Arkansas",
+  "Latitude": 34.5573549,
+  "Longitude": -92.2863402999999
+ },
+ {
+  "State Code": "CA",
+  "State": "California",
+  "Latitude": 36.778261,
+  "Longitude": -119.4179324
+ },
+ {
+  "State Code": "CO",
+  "State": "Colorado",
+  "Latitude": 39.5500507,
+  "Longitude": -105.7820674
+ },
+ {
+  "State Code": "CT",
+  "State": "Connecticut",
+  "Latitude": 41.6032207,
+  "Longitude": -73.087749
+ },
+ {
+  "State Code": "DE",
+  "State": "Delaware",
+  "Latitude": 38.9108325,
+  "Longitude": -75.5276698999999
+ },
+ {
+  "State Code": "FL",
+  "State": "Florida",
+  "Latitude": 27.6648274,
+  "Longitude": -81.5157535
+ },
+ {
+  "State Code": "GA",
+  "State": "Georgia",
+  "Latitude": 32.816729495696634,
+  "Longitude": -83.47492781142806
+ },
+ {
+  "State Code": "HI",
+  "State": "Hawaii",
+  "Latitude": 19.8967662,
+  "Longitude": -155.5827818
+ },
+ {
+  "State Code": "ID",
+  "State": "Idaho",
+  "Latitude": 44.0682019,
+  "Longitude": -114.7420408
+ },
+ {
+  "State Code": "IL",
+  "State": "Illinois",
+  "Latitude": 40.6331249,
+  "Longitude": -89.3985283
+ },
+ {
+  "State Code": "IN",
+  "State": "Indiana",
+  "Latitude": 40.2671941,
+  "Longitude": -86.1349019
+ },
+ {
+  "State Code": "IA",
+  "State": "Iowa",
+  "Latitude": 41.8780025,
+  "Longitude": -93.097702
+ },
+ {
+  "State Code": "KS",
+  "State": "Kansas",
+  "Latitude": 39.011902,
+  "Longitude": -98.4842465
+ },
+ {
+  "State Code": "KY",
+  "State": "Kentucky",
+  "Latitude": 37.8393332,
+  "Longitude": -84.2700179
+ },
+ {
+  "State Code": "LA",
+  "State": "Louisiana",
+  "Latitude": 30.9842977,
+  "Longitude": -91.9623326999999
+ },
+ {
+  "State Code": "ME",
+  "State": "Maine",
+  "Latitude": 45.253783,
+  "Longitude": -69.4454689
+ },
+ {
+  "State Code": "MD",
+  "State": "Maryland",
+  "Latitude": 39.0457549,
+  "Longitude": -76.6412711999999
+ },
+ {
+  "State Code": "MA",
+  "State": "Massachusetts",
+  "Latitude": 42.4072107,
+  "Longitude": -71.3824374
+ },
+ {
+  "State Code": "MI",
+  "State": "Michigan",
+  "Latitude": 44.3148443,
+  "Longitude": -85.6023642999999
+ },
+ {
+  "State Code": "MN",
+  "State": "Minnesota",
+  "Latitude": 46.729553,
+  "Longitude": -94.6858998
+ },
+ {
+  "State Code": "MS",
+  "State": "Mississippi",
+  "Latitude": 32.3546679,
+  "Longitude": -89.3985283
+ },
+ {
+  "State Code": "MO",
+  "State": "Missouri",
+  "Latitude": 37.9642529,
+  "Longitude": -91.8318334
+ },
+ {
+  "State Code": "MT",
+  "State": "Montana",
+  "Latitude": 46.8796822,
+  "Longitude": -110.3625658
+ },
+ {
+  "State Code": "NE",
+  "State": "Nebraska",
+  "Latitude": 41.4925374,
+  "Longitude": -99.9018131
+ },
+ {
+  "State Code": "NV",
+  "State": "Nevada",
+  "Latitude": 38.8026097,
+  "Longitude": -116.419389
+ },
+ {
+  "State Code": "NH",
+  "State": "New Hampshire",
+  "Latitude": 43.1938516,
+  "Longitude": -71.5723953
+ },
+ {
+  "State Code": "NJ",
+  "State": "New Jersey",
+  "Latitude": 40.0583238,
+  "Longitude": -74.4056612
+ },
+ {
+  "State Code": "NM",
+  "State": "New Mexico",
+  "Latitude": 34.5199402,
+  "Longitude": -105.8700901
+ },
+ {
+  "State Code": "NY",
+  "State": "New York",
+  "Latitude": 40.7127753,
+  "Longitude": -74.0059728
+ },
+ {
+  "State Code": "NC",
+  "State": "North Carolina",
+  "Latitude": 35.7595731,
+  "Longitude": -79.0192996999999
+ },
+ {
+  "State Code": "ND",
+  "State": "North Dakota",
+  "Latitude": 47.5514926,
+  "Longitude": -101.0020119
+ },
+ {
+  "State Code": "OH",
+  "State": "Ohio",
+  "Latitude": 40.4172871,
+  "Longitude": -82.907123
+ },
+ {
+  "State Code": "OK",
+  "State": "Oklahoma",
+  "Latitude": 35.0077519,
+  "Longitude": -97.092877
+ },
+ {
+  "State Code": "OR",
+  "State": "Oregon",
+  "Latitude": 43.8041334,
+  "Longitude": -120.5542012
+ },
+ {
+  "State Code": "PA",
+  "State": "Pennsylvania",
+  "Latitude": 41.2033216,
+  "Longitude": -77.1945247
+ },
+ {
+  "State Code": "RI",
+  "State": "Rhode Island",
+  "Latitude": 41.5800945,
+  "Longitude": -71.4774291
+ },
+ {
+  "State Code": "SC",
+  "State": "South Carolina",
+  "Latitude": 33.836081,
+  "Longitude": -81.1637245
+ },
+ {
+  "State Code": "SD",
+  "State": "South Dakota",
+  "Latitude": 43.9695148,
+  "Longitude": -99.9018131
+ },
+ {
+  "State Code": "TN",
+  "State": "Tennessee",
+  "Latitude": 35.5174913,
+  "Longitude": -86.5804473
+ },
+ {
+  "State Code": "TX",
+  "State": "Texas",
+  "Latitude": 31.9685988,
+  "Longitude": -99.9018131
+ },
+ {
+  "State Code": "UT",
+  "State": "Utah",
+  "Latitude": 39.3209801,
+  "Longitude": -111.0937311
+ },
+ {
+  "State Code": "VT",
+  "State": "Vermont",
+  "Latitude": 44.5588028,
+  "Longitude": -72.5778414999999
+ },
+ {
+  "State Code": "VA",
+  "State": "Virginia",
+  "Latitude": 37.4315734,
+  "Longitude": -78.6568942
+ },
+ {
+  "State Code": "WA",
+  "State": "Washington",
+  "Latitude": 38.9071923,
+  "Longitude": -77.0368707
+ },
+ {
+  "State Code": "DC",
+  "State": "Washington D.C.",
+  "Latitude": 38.9071923,
+  "Longitude": -77.0368707
+ },
+ {
+  "State Code": "WV",
+  "State": "West Virginia",
+  "Latitude": 38.5976262,
+  "Longitude": -80.4549026
+ },
+ {
+  "State Code": "WI",
+  "State": "Wisconsin",
+  "Latitude": 43.7844397,
+  "Longitude": -88.7878678
+ },
+ {
+  "State Code": "WY",
+  "State": "Wyoming",
+  "Latitude": 43.0759678,
+  "Longitude": -107.2902839
+ }
+]
